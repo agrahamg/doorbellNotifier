13a937edc849a9afe4cb373efbe313e89ac77770
Andrew Graham <andrew.grahamg@gmail.com>
2015-01-30 18:12:43 -0500

    added express to keep task running

--- index.js
+++ index.js
@@ -6,19 +6,35 @@ var hipchatter = new Hipchatter(config.hipchat.APIKey);
 var xbmc = require('xbmc-notify');
 xbmc.config(config.xbmc);
 
-hipchatter.notify(config.hipchat.roomName,
-{
-  message: 'Ring Ring Ring',
-  color: 'green',
-  token: config.hipchat.roomAPIKey
-}, function(err){
-  if (err == null) {
-    console.log('Successfully notified the room.')
-  }
-  else{
-    console.log(err);
-  };
-});
 
 
-xbmc.notify("Ring ring ring");
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  hipchatter.notify(config.hipchat.roomName,
+    {
+      message: 'Ring Ring Ring',
+      color: 'green',
+      token: config.hipchat.roomAPIKey
+    }, function(err){
+      if (err == null) {
+        console.log('Successfully notified the room.')
+      }
+      else{
+        console.log(err);
+      };
+    });
+    xbmc.notify("Ring ring ring");
+
+    res.end();
+})
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+})
